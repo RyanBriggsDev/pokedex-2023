88cd4fc030123b9142a3e6a887576b1baf9cc6ee
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-02-21 16:03:38 +0000

    page layout components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import Container from './Container'
+
+export default function Footer() {
+  return (
+    <footer className="h-10 flex items-center justify-center">
+      <Container>footer</Container>
+    </footer>
+  )
+}
