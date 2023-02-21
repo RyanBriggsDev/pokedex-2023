dd93f5db53f504634e743f89b9596ace9548c709
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-02-21 17:25:11 +0000

    added header component

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+export default function Button({ handleClick, text, type }: ButtonProps) {
+  return (
+    <button
+      className={`px-2 py-1 flex bg w-fit text-lg
+      ${
+        type === 'yellow'
+          ? 'bg-poke-yellow text-black'
+          : 'bg-poke-blue text-white'
+      }`}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  )
+}
+
+type ButtonProps = {
+  handleClick: () => void
+  text: string
+  type?: string | boolean
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'poke-blue': '#302d95',
+        'poke-yellow': '#FCCF00',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss')],
 }
